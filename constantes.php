9a26a5d59b53f18c256cbee3454faf91938e2c33
moustaphafaye <99656107+moustaphafaye@users.noreply.github.com>
2022-03-04 08:56:56 +0000

    Add files via upload

---
+++ constantes.php
@@ -0,0 +1,28 @@
+<?php
+define("ROOT",str_replace("public".DIRECTORY_SEPARATOR."index.php","",$_SERVER['SCRIPT_FILENAME']));
+// echo $_SERVER['SCRIPT_FILENAME'];die();
+/**
+* Chemin sur dossier src qui contient les controllers et les modeles
+*/
+define("PATH_SRC",ROOT."src".DIRECTORY_SEPARATOR);
+/**
+* Chemin sur dossier templates du projet
+*/
+define("PATH_VIEWS",ROOT."templates".DIRECTORY_SEPARATOR);
+/**
+* Chemin sur data qui contient le fichier Json db.json
+*/
+define("PATH_DB",ROOT."data".DIRECTORY_SEPARATOR."db.json");
+/**
+* Chemin sur le dossier public , pour inclusion des images,css et js
+*/
+define("WEB_ROOT",str_replace("index.php","",$_SERVER['SCRIPT_NAME']));
+/**
+* Requettes POST et GET
+*/
+define("WEB_ROOT","http://localhost:8002/");
+define("WEB_PUBLIC",str_replace("index.php","",$_SERVER['SCRIPT_NAME']));
+/*clee d'erreur*/
+define("KEY_ERRORS","errors");
+/*le clee de l'utilisateur*/
+define("KEY_USER_CONNEXION","user-connect");
---
+++ router.php
@@ -0,0 +1,18 @@
+<?php
+    if(isset($_REQUEST['controller']) ){
+        switch ($_REQUEST['controller']) {
+            case "securite" :
+                require_once(PATH_SRC."controllers/securite.controllers.php");
+                dit("dhf");
+            break;
+            case "user" :
+                require_once(PATH_SRC."controllers/user.controllers.php");
+            break;
+            default:
+                echo 'votre choix est invalide';
+            break;
+        }
+        }
+     else{
+        require_once(PATH_SRC."controllers/securite.controllers.php");
+    }
